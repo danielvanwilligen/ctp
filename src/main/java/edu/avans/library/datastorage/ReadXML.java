9d898bf2dfdc306054cb292f95811f2d0ef70c18
danielvanwilligen <danielvanwilligen@online.nl>
2016-10-13 21:01:53 +0200

    test

--- src/main/java/edu/avans/library/datastorage/ReadXML.java
+++ src/main/java/edu/avans/library/datastorage/ReadXML.java
@@ -32,39 +32,37 @@ import org.w3c.dom.NodeList;
  */
 
 public class ReadXML {
-    
     public void getXMLData() {
     // read xml
     try {
-        File fXmlFile = new File(SetPath.stepdataresource+name+"/xml/"+name+".xml");
-        DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
-        DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
-        Document doc = dBuilder.parse(fXmlFile);
-        doc.getDocumentElement().normalize();
-        NodeList nList = doc.getElementsByTagName("xml");
+	File fXmlFile = new File(SetPath.stepdataresource+name+"/xml/"+name+".xml");
+	DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+	DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+	Document doc = dBuilder.parse(fXmlFile);
+	doc.getDocumentElement().normalize();
+	NodeList nList = doc.getElementsByTagName("xml");
                 
-        for (int temp = 0; temp < nList.getLength(); temp++) {
-        Node nNode = nList.item(temp);
-        if (nNode.getNodeType() == Node.ELEMENT_NODE) {
-            Element eElement = (Element) nNode;
-            specnrres.setText(eElement.getElementsByTagName("spec").item(0).getTextContent());
-            ordernrres.setText(eElement.getElementsByTagName("ordernumber").item(0).getTextContent());
-            customerres.setText(eElement.getElementsByTagName("customer").item(0).getTextContent());
-            widthres.setText(eElement.getElementsByTagName("cylinderwidth").item(0).getTextContent());
-            heightres.setText(eElement.getElementsByTagName("cylinderheight").item(0).getTextContent());
-            itemsxres.setText(eElement.getElementsByTagName("items_on_cylinder_x").item(0).getTextContent());
-            itemsyres.setText(eElement.getElementsByTagName("items_on_cylinder_y").item(0).getTextContent());
-            mfgres.setText(eElement.getElementsByTagName("mfg").item(0).getTextContent());
-            singleres.setText(eElement.getElementsByTagName("single").item(0).getTextContent());
-            dieshaperes.setText(eElement.getElementsByTagName("bleed").item(0).getTextContent());
-            bleedres.setText(eElement.getElementsByTagName("dieshape").item(0).getTextContent());
-            itemsxresvalue = eElement.getElementsByTagName("items_on_cylinder_x").item(0).getTextContent();
-            itemsyresvalue = eElement.getElementsByTagName("items_on_cylinder_y").item(0).getTextContent();
-        }
-        }
+	for (int temp = 0; temp < nList.getLength(); temp++) {
+	    Node nNode = nList.item(temp);
+	    if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+		Element eElement = (Element) nNode;
+		specnrres.setText(eElement.getElementsByTagName("spec").item(0).getTextContent());
+		ordernrres.setText(eElement.getElementsByTagName("ordernumber").item(0).getTextContent());
+		customerres.setText(eElement.getElementsByTagName("customer").item(0).getTextContent());
+		widthres.setText(eElement.getElementsByTagName("cylinderwidth").item(0).getTextContent());
+		heightres.setText(eElement.getElementsByTagName("cylinderheight").item(0).getTextContent());
+		itemsxres.setText(eElement.getElementsByTagName("items_on_cylinder_x").item(0).getTextContent());
+		itemsyres.setText(eElement.getElementsByTagName("items_on_cylinder_y").item(0).getTextContent());
+		mfgres.setText(eElement.getElementsByTagName("mfg").item(0).getTextContent());
+		singleres.setText(eElement.getElementsByTagName("single").item(0).getTextContent());
+		dieshaperes.setText(eElement.getElementsByTagName("bleed").item(0).getTextContent());
+		bleedres.setText(eElement.getElementsByTagName("dieshape").item(0).getTextContent());
+		itemsxresvalue = eElement.getElementsByTagName("items_on_cylinder_x").item(0).getTextContent();
+		itemsyresvalue = eElement.getElementsByTagName("items_on_cylinder_y").item(0).getTextContent();
+	    }
+	}
     } catch (Exception e) {
 	LOGGER.info((Supplier<String>) e);
     }
     }
-
 }
