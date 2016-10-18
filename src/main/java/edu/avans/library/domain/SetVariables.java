ffc1b2ed8a5a42f3fd5442fcaf9599793ec0ae06
danielvanwilligen <danielvanwilligen@online.nl>
2016-10-18 15:44:08 +0200

    test

---
+++ src/main/java/edu/avans/library/domain/SetVariables.java
@@ -0,0 +1,23 @@
+package edu.avans.library.domain;
+
+import java.awt.Image;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class SetVariables {
+    public static JFrame frame;
+    public static JPanel bottompanel, subbottompanel, toppanel, leftpanel, centerpanel, subcenterpanel, rightpanel, subrightpanel, rightbuttonpanel, custom;    
+    public static JLabel img, specnrres, ordernrres, customerres, widthres, heightres, itemsxres, itemsyres, mfgres, singleres, dieshaperes, bleedres, specbuttonlocked, savebuttonlocked;
+    public static JButton specbutton, savebutton, resetbutton, autobutton;
+    public static ImageIcon placeimage;
+    public static Image draggedimage;
+    public static String[] imagesequencearray, cleanfirstimage;
+    public static String itemsxresvalue, itemsyresvalue, pathnameimage, xmloutput, imagesequence, draggedimagename, name, datestamp;
+
+    public static double itemsover, totalgridfieldsint, itemstoplaceint;
+    public static int gridplacecounter, childcounter, newimagewidth, newimageheight, x, y, navpanel, navsidepanel, screenwidth, screenheight, currentimagewidth, currentimageheight, newimageheigth, centerpanelwidth, centerpanelheight, itemsxresvalueint, itemsyresvalueint, setgridx, setgridy, newpanelwidth, newpanelheight, currentblancoimagewidth, currentblancoimageheight, newblancoimagewidth, newblancoimageheigth, savecounter, dragdropplacecounter;
+    public static boolean menustatus;
+}
