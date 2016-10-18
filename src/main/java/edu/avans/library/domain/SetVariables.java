af255fdb01ae4c8222dfb39f9a92f1cd5c2a245d
danielvanwilligen <danielvanwilligen@online.nl>
2016-10-18 15:46:48 +0200

    test

--- src/main/java/edu/avans/library/domain/SetVariables.java
+++ src/main/java/edu/avans/library/domain/SetVariables.java
@@ -8,16 +8,16 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class SetVariables {
-    public static JFrame frame;
-    public static JPanel bottompanel, subbottompanel, toppanel, leftpanel, centerpanel, subcenterpanel, rightpanel, subrightpanel, rightbuttonpanel, custom;    
-    public static JLabel img, specnrres, ordernrres, customerres, widthres, heightres, itemsxres, itemsyres, mfgres, singleres, dieshaperes, bleedres, specbuttonlocked, savebuttonlocked;
-    public static JButton specbutton, savebutton, resetbutton, autobutton;
-    public static ImageIcon placeimage;
-    public static Image draggedimage;
-    public static String[] imagesequencearray, cleanfirstimage;
-    public static String itemsxresvalue, itemsyresvalue, pathnameimage, xmloutput, imagesequence, draggedimagename, name, datestamp;
+    static JFrame frame;
+    static JPanel bottompanel, subbottompanel, toppanel, leftpanel, centerpanel, subcenterpanel, rightpanel, subrightpanel, rightbuttonpanel, custom;    
+    static JLabel img, specnrres, ordernrres, customerres, widthres, heightres, itemsxres, itemsyres, mfgres, singleres, dieshaperes, bleedres, specbuttonlocked, savebuttonlocked;
+    static JButton specbutton, savebutton, resetbutton, autobutton;
+    static ImageIcon placeimage;
+    static Image draggedimage;
+    static String[] imagesequencearray, cleanfirstimage;
+    static String itemsxresvalue, itemsyresvalue, pathnameimage, xmloutput, imagesequence, draggedimagename, name, datestamp;
 
-    public static double itemsover, totalgridfieldsint, itemstoplaceint;
-    public static int gridplacecounter, childcounter, newimagewidth, newimageheight, x, y, navpanel, navsidepanel, screenwidth, screenheight, currentimagewidth, currentimageheight, newimageheigth, centerpanelwidth, centerpanelheight, itemsxresvalueint, itemsyresvalueint, setgridx, setgridy, newpanelwidth, newpanelheight, currentblancoimagewidth, currentblancoimageheight, newblancoimagewidth, newblancoimageheigth, savecounter, dragdropplacecounter;
-    public static boolean menustatus;
+    static double itemsover, totalgridfieldsint, itemstoplaceint;
+    static int gridplacecounter, childcounter, newimagewidth, newimageheight, x, y, navpanel, navsidepanel, screenwidth, screenheight, currentimagewidth, currentimageheight, newimageheigth, centerpanelwidth, centerpanelheight, itemsxresvalueint, itemsyresvalueint, setgridx, setgridy, newpanelwidth, newpanelheight, currentblancoimagewidth, currentblancoimageheight, newblancoimagewidth, newblancoimageheigth, savecounter, dragdropplacecounter;
+    static boolean menustatus;
 }
