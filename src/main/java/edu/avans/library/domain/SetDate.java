724ecabf88c3fb42058f2071e54094ca636256bc
danielvanwilligen <danielvanwilligen@online.nl>
2016-10-19 10:42:46 +0200

    test

--- src/main/java/edu/avans/library/domain/SetDate.java
+++ src/main/java/edu/avans/library/domain/SetDate.java
@@ -32,7 +32,115 @@ public class SetDate {
     public SetDate(String settimestamp) {
         timestamp= settimestamp;
     }
-        
+    
+    /**
+    * getYear
+    * getYear get year value 
+    * @return 
+    */
+    public String getYear(){
+        Date currentDate = new Date();
+        date = currentDate.toString();
+        String [] splitdate = date.split(" ");
+        year = splitdate[FIVE];
+        return year;
+    }
+    
+    /**
+    * getMonth
+    * getMonth get month value 
+    * @return 
+    */
+    public String getMonth(){
+        Date currentDate = new Date();
+        date = currentDate.toString();
+        String [] splitdate = date.split(" ");
+        month = splitdate[1];
+        return month;
+    }
+    
+    /**
+    * getDay
+    * getDay get day value 
+    * @return 
+    */
+    public String getDay(){
+        Date currentDate = new Date();
+        date = currentDate.toString();
+        String [] splitdate = date.split(" ");
+        day = splitdate[TWO];
+        return day;
+    }
+
+    /**
+    * getHour
+    * getHour get hours value 
+    * @return 
+    */
+    public String getHour(){
+        Date currentDate = new Date();
+        date = currentDate.toString();
+        String [] splitdate = date.split(" ");
+        time = splitdate[THREE];
+        String [] splittime = time.split(":");
+        hour = splittime[0];
+        return hour;
+    }
+
+    /**
+    * getMinutes
+    * getMinutes get minutes value 
+    * @return 
+    */
+    public String getMinutes(){
+        Date currentDate = new Date();
+        date = currentDate.toString();
+        String [] splitdate = date.split(" ");
+        time = splitdate[THREE];
+        String [] splittime = time.split(":");
+        minutes = splittime[0];
+        return minutes;
+    }
+
+    /**
+    * getSeconds
+    * getSeconds get seconds value 
+    * @return 
+    */
+    public String getSeconds(){
+        Date currentDate = new Date();
+        date = currentDate.toString();
+        String [] splitdate = date.split(" ");
+        time = splitdate[THREE];
+        String [] splittime = time.split(":");
+        seconds = splittime[0];
+        return seconds;
+    }
+    
+    public void setYear(String setYear) {
+        year = setYear;
+    }
+    
+    public void setMonth(String setMonth) {
+        month = setMonth;
+    }
+    
+    public void setDay(String setDay) {
+        day = setDay;
+    }
+    
+    public void setHour(String setHour) {
+        hour = setHour;
+    }
+    
+    public void setMinutes(String setMinutes) {
+        minutes = setMinutes;
+    }
+    
+    public void setSeconds(String setSeconds) {
+        seconds = setSeconds;
+    }
+    
     /**
     * timestamp toString
     * timestamp toString gives back a timestamp 
